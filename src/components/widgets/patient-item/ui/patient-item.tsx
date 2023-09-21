24ed8984c2e32f8d4758d4e15929b22d18402b18
Vadim Belousov <thevadiratti@yandex.ru>
2023-09-21 13:36:03 +0500

    feat: patient-item

---
+++ src/components/widgets/patient-item/ui/patient-item.tsx
@@ -0,0 +1,36 @@
+import { memo, useCallback } from "react";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { PatientItemInfo, setCurrent } from "@/components/entities/patient";
+import { StaticImageData } from "next/image";
+import Styles from "./patient-item.module.scss";
+
+interface Props {
+  name: string;
+  avatar: StaticImageData;
+  id: string;
+}
+
+const PatientItem = memo(({ name, avatar, id }: Props) => {
+  const current = useAppSelector((state) => state.patientsSlice.current);
+  const dispatch = useAppDispatch();
+
+  const isCurrent = current?.id === id;
+
+  const handleClick = useCallback(() => {
+    dispatch(setCurrent(id));
+  }, [id, dispatch]);
+
+  return (
+    <li className={Styles.item}>
+      <button
+        className={`${Styles.button} ${isCurrent && Styles.active}`}
+        type="button"
+        onClick={handleClick}
+      >
+        <PatientItemInfo name={name} avatar={avatar} />
+      </button>
+    </li>
+  );
+});
+
+export default PatientItem;
