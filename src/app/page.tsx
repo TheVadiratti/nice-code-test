2829a9da46d7de03b379ef54f0876cf23a100194
Vadim Belousov <thevadiratti@yandex.ru>
2023-09-22 11:14:57 +0500

    feat: автоустановка текущего пациента

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import PatientList from "@/components/widgets/patient-list";
 import PATIENTS from "@/fake-data";
-import { setPatients } from "@/components/entities/patient";
+import { setCurrent, setPatients } from "@/components/entities/patient";
 import PatientDataHeader from "@/components/widgets/patient-data-header";
 import PatientListHeader from "@/components/widgets/patient-list-header";
 import PatientDataMain from "@/components/widgets/patient-data";
@@ -15,6 +15,7 @@ export default function Home() {
   const dispatch = useAppDispatch();
   useEffect(() => {
     dispatch(setPatients(PATIENTS));
+    dispatch(setCurrent(PATIENTS[0].id));
   }, [dispatch]);
 
   return (
