646ddb6a88bd72a57ab111fe3e7c8b4c62ccfd64
Vadim Belousov <thevadiratti@yandex.ru>
2023-09-22 10:59:45 +0500

    feat: patient-data-main

--- src/components/widgets/patient-data/ui/patient-data-main/patient-data-main.tsx
+++ src/components/widgets/patient-data/ui/patient-data-main/patient-data-main.tsx
@@ -1,32 +1,67 @@
 import { memo, useMemo } from "react";
-import { PatientDataPages } from "@/components/features/navigation";
-import Styles from "./patient-data-main.module.scss";
 import { useAppSelector } from "@/store/hooks";
+import Note from "@/components/entities/note";
+import Consultation from "@/components/entities/consultation";
+import Video from "@/components/entities/video";
+import Event from "@/components/entities/event";
+import Styles from "./patient-data-main.module.scss";
 
-interface Props {
-  page: PatientDataPages;
-}
-
-const PatientDataMain = memo(({ page }: Props) => {
+const PatientDataMain = memo(() => {
   const currentPatient = useAppSelector((state) => state.patientsSlice.current);
-
-  // const content = useMemo(() => {
-  //   switch (page) {
-  //     case "events": {
-
-  //     }
-  //     default: {
-  //       return null;
-  //     }
-  //   }
-  // }, []);
-
-  return (
-    <div className={Styles.cnt}>
-      {/* {content && content} */}
-      
-    </div>
+  const currentPage = useAppSelector(
+    (state) => state.navigationSlice.currentPage,
   );
+
+  const content = useMemo(() => {
+    switch (currentPage) {
+      case "notes": {
+        return currentPatient?.notes.map((note) => (
+          <Note text={note.text} date={note.date} image={note.image} />
+        ));
+      }
+      case "consultations": {
+        return currentPatient?.consultations.map((consultation) => (
+          <Consultation
+            name={consultation.type}
+            date={consultation.date}
+            status={consultation.status}
+          />
+        ));
+      }
+      case "videos": {
+        return currentPatient?.videos.map((video) => (
+          <Video
+            name={video.name}
+            date={video.dates}
+            image={video.img}
+            author={video.author}
+          />
+        ));
+      }
+      case "events": {
+        return currentPatient?.events.map((event) => (
+          <Event
+            name={event.name}
+            type={event.type}
+            img={event.img}
+            date={event.date}
+            time={event.time}
+          />
+        ));
+      }
+      default: {
+        return null;
+      }
+    }
+  }, [
+    currentPage,
+    currentPatient?.consultations,
+    currentPatient?.events,
+    currentPatient?.notes,
+    currentPatient?.videos,
+  ]);
+
+  return <ul className={Styles.cnt}>{content && content}</ul>;
 });
 
 export default PatientDataMain;
